547bbb2272a7bf928bc29ae4eafbff5f93415071
Pawan-Kumar-56 <raosabh1981@gmail.com>
2026-09-17 17:58:29 +0530

    Time: 4 ms (73.48%), Space: 45.5 MB (21.84%) - LeetHub

--- 0496-next-greater-element-i/0496-next-greater-element-i.java
+++ 0496-next-greater-element-i/0496-next-greater-element-i.java
@@ -3,30 +3,65 @@ class Solution {
         int n=nums1.length;
         int m=nums2.length;
         int[] ans=new int[n];
-        int k=0;
+        Stack<Integer> st=new Stack<>();
+        HashMap<Integer,Integer> map=new HashMap<>();
+        for(int num:nums2){
+            while(!st.isEmpty() && st.peek()<num){
+                map.put(st.pop(),num);
+            }
+            st.push(num);
+        }
+        while(!st.isEmpty()){
+            map.put(st.pop(),-1);
+        }
         for(int i=0;i<n;i++){
-            int j=0;
-            while(nums1[i]!=nums2[j]){
-                j++;
-            }
-            j++;
-            int b=-2;
-            while(j<m){
-                if(nums2[j]>nums1[i]){
-                    ans[k]=nums2[j];
-                    k++;
-                    b=0;
-                    break;
-                }
-                j++;
-            }
-            if(b==-2){
-                ans[k]=-1;
-                k++;
-            }
-            
-
+            ans[i]=map.get(nums1[i]);
         }
         return ans;
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+        // int n=nums1.length;
+        // int m=nums2.length;
+        // int[] ans=new int[n];
+        // int k=0;
+        // for(int i=0;i<n;i++){
+        //     int j=0;
+        //     while(nums1[i]!=nums2[j]){
+        //         j++;
+        //     }
+        //     j++;
+        //     int b=-2;
+        //     while(j<m){
+        //         if(nums2[j]>nums1[i]){
+        //             ans[k]=nums2[j];
+        //             k++;
+        //             b=0;
+        //             break;
+        //         }
+        //         j++;
+        //     }
+        //     if(b==-2){
+        //         ans[k]=-1;
+        //         k++;
+        //     }
+            
+
+        // }
+        // return ans;
     }
 }
